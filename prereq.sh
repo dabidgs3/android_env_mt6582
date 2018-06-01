69c02891bbbc0e1d7d5ee62e9ad5378584fb5a5e
dguzman <david.alfonso.guzman.cabascango@fokus-extern.fraunhofer.de>
2018-06-01 15:19:11 +0200

    dev:server version for ubuntu installation env

--- prereq.sh
+++ prereq.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Ubuntu 16 - server 
 sudo apt update 
 sudo apt install openjdk-7-jdk openjdk-7-jre
 sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev  openjdk-7-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib liblz4-* android-tools-adb android-tools-fastboot ccache
