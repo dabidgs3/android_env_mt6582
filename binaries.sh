7e3cdacc99060ba606239d09f2fcf45ad4ab6ead
dguzman <david.alfonso.guzman.cabascango@fokus-extern.fraunhofer.de>
2018-06-02 15:09:40 +0200

    env: cache

--- binaries.sh
+++ binaries.sh
@@ -2,3 +2,11 @@ mkdir ~/bin
 PATH=~/bin:$PATH
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
+git config --global user.email "dabidgs3@hotmail.com"
+git config --global user.name "dabidgs3"
+gksu gedit ~/.bashrc
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+source .bashrc
+ccache -M 5G
+
