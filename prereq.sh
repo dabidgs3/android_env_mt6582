b44479e49d651aa5204a3df531bf10f61deb53a1
dguzman <david.alfonso.guzman.cabascango@fokus-extern.fraunhofer.de>
2018-06-02 13:08:54 +0200

    dev: jdk repo

--- prereq.sh
+++ prereq.sh
@@ -2,6 +2,7 @@
 #Ubuntu 16 - server 
 #echo "deb http://archive.ubuntu.com/ubuntu wily main universe" | sudo tee /etc/apt/sources.list.d/wily-copies.list
 sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo add-apt-repository ppa:openjdk-r/ppa  
 sudo apt update 
 sudo apt install openjdk-7-jdk openjdk-7-jre
 sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev  openjdk-7-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib liblz4-* android-tools-adb android-tools-fastboot ccache
